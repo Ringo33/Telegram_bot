3b82a0aa01bb4fed86e10cace09e6ce509ecf136
Rinat <masagutov33@gmail.com>
2022-09-14 22:17:43 +0300

    commit

--- new.py
+++ new.py
@@ -1,3 +1,5 @@
+import pprint
+
 import requests
 
 
@@ -14,14 +16,22 @@ def test():
 
 def test2():
     headers = {
-        'Authorization': 'Token 44b77bb6694dc9e24638e60fe167f2fe753dd304'
+        'Authorization': 'Token 80c65edde52d43a8ab260c261a7e41917860f814'
+        # 'Authorization': 'Token d0b6e0e65976a1090dac7ca71882d3698aa8adfe'
+        # 'Authorization': 'Token 44b77bb6694dc9e24638e60fe167f2fe753dd304'
+
     }
     payload = {
-        "text": "Проверка 2"
+        "text": "re-re тест",
+        "category": "2",
+        "slug": "sup-harcho1",
+        "title": "Суп-харчо1"
     }
-    response = requests.post('http://127.0.0.1:8000/api/v1/posts/', headers=headers, params=payload)
-    print(response.text)
-    return response
+    response = requests.patch('http://127.0.0.1:8000/api/v1/posts/93/comments/64/', headers=headers, data=payload)
 
+    # for i in range(len(response)):
+    #     print(response[i])
+    # return response
+    pprint.pprint(response.text)
 
 test2()
